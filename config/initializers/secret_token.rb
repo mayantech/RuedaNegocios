8dbcf4dc6f8ccf53ba9a9fd7f6f6759866e0eb21
Carlos Ramirez <carlos.ramirez@mayan-tech.com>
2014-04-30 21:40:11 -0600

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RuedaNegocios::Application.config.secret_key_base = '691c5690ee0f64c72b8b7241981823d63ed0ff3cbbd9e3cebc4f929287f4d3cb6cf050a1449f89ef2f20ced9369938f93ec05c5e7eadf3a36c34167b2a40b390'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RuedaNegocios::Application.config.session_store :cookie_store, key: '_RuedaNegocios_session'
